331a40c55902c47b5d81b203e983e4beaf1b593a
hukai <huzhengen@163.com>
2021-04-16 16:53:17 +0800

    add jquery and bootstrap

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now['danger'] = '邮箱或密码不正确'
       render 'new'
