061ae36dc2360852620c3b2b6ad0f998b73d9241
hukai <huzhengen@163.com>
2021-04-20 16:03:41 +0800

    redirect to login_url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   attr_accessor :name, :email
 
   def new
@@ -39,5 +40,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = '请先登录'
+      redirect_to login_url
+    end
   end
 end
